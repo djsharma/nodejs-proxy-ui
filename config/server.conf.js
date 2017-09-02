96ca2eae0cd94d25a2b6a85c956e788d9be14a0b
Dhananjay Sharma <dhananjaysharma@Dhananjays-MacBook-Air.local>
2017-09-02 16:00:05 +0530

    [add server config]

---
+++ config/server.conf.js
@@ -0,0 +1,11 @@
+/*
+* @Author: dhananjaysharma
+* @Date:   2017-09-02 15:57:32
+* @Last Modified by:   dhananjaysharma
+* @Last Modified time: 2017-09-02 15:58:32
+*/
+'use strict'
+
+exports.dev={
+	SERVER_PORT: process.env.PORT || 8080
+}
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 * @Author: dhananjaysharma
 * @Date:   2017-09-02 03:03:30
 * @Last Modified by:   dhananjaysharma
-* @Last Modified time: 2017-09-02 15:18:36
+* @Last Modified time: 2017-09-02 15:59:33
 */
 
 'use strict';
@@ -11,10 +11,11 @@ var http = require('http');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var path = require('path');
+var config = require('./config/server.conf');
 
 //create app for express
 var app = express();
 //server initialization on express
-var server = app.listen(8080,function (arg) {
+var server = app.listen(config.dev.SERVER_PORT,function (arg) {
 		console.log('proxy-node-ui-server listening PORT: '+ server.address().port);
 	});
