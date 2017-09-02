3dd4d36ce6db20456d26de06fc389a9f67f34044
Dhananjay Sharma <dhananjaysharma@Dhananjays-MacBook-Air.local>
2017-09-02 15:15:06 +0530

    [add cookie-parser]

--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 * @Author: dhananjaysharma
 * @Date:   2017-09-02 03:03:30
 * @Last Modified by:   dhananjaysharma
-* @Last Modified time: 2017-09-02 15:13:10
+* @Last Modified time: 2017-09-02 15:14:08
 */
 
 'use strict';
 var express = require('express');
 var http = require('http');
+var cookieParser = require('cookie-parser');
