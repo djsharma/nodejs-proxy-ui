40dfe817f03120499812baf52bf717a46afa3fa5
Dhananjay Sharma <dhananjaysharma@Dhananjays-MacBook-Air.local>
2017-09-02 15:19:17 +0530

    [add server listening]

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 * @Author: dhananjaysharma
 * @Date:   2017-09-02 03:03:30
 * @Last Modified by:   dhananjaysharma
-* @Last Modified time: 2017-09-02 15:16:30
+* @Last Modified time: 2017-09-02 15:18:36
 */
 
 'use strict';
@@ -11,3 +11,10 @@ var http = require('http');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var path = require('path');
+
+//create app for express
+var app = express();
+//server initialization on express
+var server = app.listen(8080,function (arg) {
+		console.log('proxy-node-ui-server listening PORT: '+ server.address().port);
+	});
