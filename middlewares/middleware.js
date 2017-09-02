1cdfb4b228b263c1c6e5e65ed086ad02c5b06014
Dhananjay Sharma <dhananjaysharma@Dhananjays-MacBook-Air.local>
2017-09-02 17:00:58 +0530

    [update middleware]

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -2,10 +2,9 @@
 * @Author: dhananjaysharma
 * @Date:   2017-09-02 16:04:43
 * @Last Modified by:   dhananjaysharma
-* @Last Modified time: 2017-09-02 16:04:57
+* @Last Modified time: 2017-09-02 17:00:33
 */
-//probable proxy api handling here
+
 exports.middle = function (request,response,next){
-	// console.log('middleware checked');
 	next();
 }
