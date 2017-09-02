6fc34d6f0f668e8cdf85066d3341db7238823c60
Dhananjay Sharma <dhananjaysharma@Dhananjays-MacBook-Air.local>
2017-09-02 16:03:02 +0530

    [middleware logger add]

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 * @Author: dhananjaysharma
 * @Date:   2017-09-02 03:03:30
 * @Last Modified by:   dhananjaysharma
-* @Last Modified time: 2017-09-02 15:59:33
+* @Last Modified time: 2017-09-02 16:02:20
 */
 
 'use strict';
@@ -12,9 +12,20 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var path = require('path');
 var config = require('./config/server.conf');
+var logger = require('morgan');
 
 //create app for express
 var app = express();
+var router = express.Router();
+
+//server middlware hook
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//logger to requests
+app.use(logger('combined'));
+
 //server initialization on express
 var server = app.listen(config.dev.SERVER_PORT,function (arg) {
 		console.log('proxy-node-ui-server listening PORT: '+ server.address().port);
