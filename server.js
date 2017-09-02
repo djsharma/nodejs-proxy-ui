942e0cffc573b5af79c2dc63a94151bbe499b8cd
Dhananjay Sharma <dhananjaysharma@Dhananjays-MacBook-Air.local>
2017-09-02 15:13:33 +0530

    [add nodemon and http]

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 * @Author: dhananjaysharma
 * @Date:   2017-09-02 03:03:30
 * @Last Modified by:   dhananjaysharma
-* @Last Modified time: 2017-09-02 15:01:56
+* @Last Modified time: 2017-09-02 15:13:10
 */
 
 'use strict';
 var express = require('express');
+var http = require('http');
