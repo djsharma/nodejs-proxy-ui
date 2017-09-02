f63cf9d30dd42ec441b6725cc3dfbf06b69e14e0
Dhananjay Sharma <dhananjaysharma@Dhananjays-MacBook-Air.local>
2017-09-02 15:16:58 +0530

    [add path module]

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 * @Author: dhananjaysharma
 * @Date:   2017-09-02 03:03:30
 * @Last Modified by:   dhananjaysharma
-* @Last Modified time: 2017-09-02 15:15:19
+* @Last Modified time: 2017-09-02 15:16:30
 */
 
 'use strict';
@@ -10,3 +10,4 @@ var express = require('express');
 var http = require('http');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var path = require('path');
